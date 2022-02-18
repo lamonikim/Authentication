7e06a0efe77c86e7570ad883ac69aeb299dd57eb
lamonikim <77967939+lamonikim@users.noreply.github.com>
2022-02-18 12:23:38 -0500

    Add files via upload
    updated assignment 1 and created base for the assignment 2

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+/* //Assignment: Assignment 2 Express Portfolio – Authentication 
+// Laura Amangeldiyeva - student id: 301167661 
+// Date: 2022/02/15 */
+module.exports =
+{
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://Nandini:zvkyHdcQYu1oheGX@mongodbserver.1pggn.mongodb.net/business_contacts?retryWrites=true&w=majority"
+}
